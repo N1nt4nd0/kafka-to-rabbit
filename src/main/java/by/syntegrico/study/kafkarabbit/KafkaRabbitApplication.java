49910efce3596a1ba15fd8535030276f4eca077a
Slava Fedorov <viacheslav.fedorov@syntegrico.by>
2024-12-05 12:30:52 +0300

    Initial commit

---
+++ src/main/java/by/syntegrico/study/kafkarabbit/KafkaRabbitApplication.java
@@ -0,0 +1,13 @@
+package by.syntegrico.study.kafkarabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/syntegrico/study/kafkarabbit/KafkaRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package by.syntegrico.study.kafkarabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
