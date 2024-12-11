406c7e93a4bcb65ae096b4579aea22c5cbdf7168
Slava Fedorov <viacheslav.fedorov@syntegrico.by>
2024-12-11 09:16:04 +0300

    Excluding all ternary operators

--- src/main/java/org/kafka/practice/kafkademo/domain/utils/LogHelper.java
+++ src/main/java/org/kafka/practice/kafkademo/domain/utils/LogHelper.java
@@ -8,7 +8,10 @@ public class LogHelper {
     public static void logError(@NonNull final String prefix,
                                 @NonNull final Throwable root,
                                 @NonNull final Logger logger) {
-        final var cause = root.getCause() == null ? root : root.getCause();
+        var cause = root.getCause();
+        if (cause == null) {
+            cause = root;
+        }
         logger.error("{} error occurred: {}({})", prefix, cause.getClass().getName(), cause.getMessage());
         logger.trace("Error details:", cause);
     }
