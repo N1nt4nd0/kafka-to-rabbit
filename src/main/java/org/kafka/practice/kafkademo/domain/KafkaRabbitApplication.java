dfef5b636b194f31c4ae243f08b235ff67631479
Slava Fedorov <viacheslav.fedorov@syntegrico.by>
2024-12-11 17:39:53 +0300

    First changes after review

--- src/main/java/org/kafka/practice/kafkademo/domain/KafkaRabbitApplication.java
+++ src/main/java/org/kafka/practice/kafkademo/domain/KafkaRabbitApplication.java
@@ -12,11 +12,7 @@ public class KafkaRabbitApplication {
     // TODO remove @Setter data from entities, make setters protected
     // TODO liquidBase changelog in sql files
     // TODO rabbit make one exchange, two queues and two routing keys
-    // TODO remove unusable sets from persons page
-    // TODO make first column bg color by css
-    // TODO change buttons in persons page (First page, Last page | Prev page, Next page)
-    // TODO use ternary operators in velocity page
-    // TODO add application.yml configurations for another profiles
+    // TODO make json rest controller request for updating persons data
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaRabbitApplication.class, args);
--- src/main/java/org/kafka/practice/kafkademo/domain/utils/LogHelper.java
+++ src/main/java/org/kafka/practice/kafkademo/domain/utils/LogHelper.java
@@ -8,7 +8,7 @@ public class LogHelper {
     public static void logError(@NonNull final String message,
                                 @NonNull final Throwable throwable,
                                 @NonNull final Logger logger) {
-        logger.error(message, throwable.getMessage());
+        logger.error("{}: {}. Cause: {}", message, throwable, String.valueOf(throwable.getCause()));
         logger.trace("Error details:", throwable);
     }
 
